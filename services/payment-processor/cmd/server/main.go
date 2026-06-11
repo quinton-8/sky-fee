1df021759a9816b9dbb44203168323e69d5ac43c
quinton-8 <jumazquinton.jq@gmail.com>
2026-06-11 12:53:21 +0300

    Add feat: load configuration variables

---
+++ services/payment-processor/cmd/server/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/sky-fee/payment-processor/internal/db"
+	"github.com/sky-fee/payment-processor/internal/handlers"
+	"github.com/sky-fee/payment-processor/internal/lightning"
+	"github.com/sky-fee/payment-processor/internal/mpesa"
+)
+
+func main() {
+	log.Println("🚀 Starting SkyFee Payment Processor Server...")
+
+	// 1. Load Configurations from Environment Variables
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	dbURL := os.Getenv("DATABASE_URL")
+	lnbitsURL := os.Getenv("LNBITS_URL")
+	lnbitsKey := os.Getenv("LNBITS_INVOICE_KEY")
+
+	
+}
